bc202c994dc31d376c8ee241e10276ab58d9d9fa
JeB <meltedspark@gmail.com>
2019-08-26 17:46:20 +0300

    background script not built

---
+++ src/background.ts
@@ -0,0 +1,7 @@
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.webNavigation.onCompleted.addListener(() => {
+        chrome.tabs.query({ active: true, currentWindow: true }, ([{ id }]) => {
+            chrome.pageAction.show(id);
+        });
+    }, { url: [{ urlMatches: 'google.com' }] });
+});
