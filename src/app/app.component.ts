4785f7689d0e0ed573b2426ea225e533e87ccb3b
JeB <meltedspark@gmail.com>
2019-08-27 17:40:07 +0300

    save last chosen color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   color: string;
+
+  ngOnInit(): void {
+    chrome.storage.sync.get('color', ({ color }) => {
+      this.color = color;
+    });
+  }
+
+  public updateColor(color: string) {
+    chrome.storage.sync.set({ color });
+  }
 
   public colorize() {
     chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
