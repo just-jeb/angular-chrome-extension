7fab4af2198a4125bcf8cdac8b71f06c046351e7
JeB <work@justjeb.com>
2022-06-23 13:01:41 +0300

    manifest v3, no background page live reload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+const updateBackgroundColor = (color: string) => document.body.style.backgroundColor = color;
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -15,22 +18,16 @@ export class AppComponent implements OnInit {
   }
 
   public updateColor(color: string) {
-    chrome.storage.sync.set({ color});
+    chrome.storage.sync.set({ color });
   }
 
   public colorize() {
-      chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-        chrome.tabs.executeScript(
-          tabs[0].id!,
-          { code: `document.body.style.backgroundColor = '${ this.color }';` }
-        );
+    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+      chrome.scripting.executeScript({
+        target: { tabId: tabs[0].id! },
+        func: updateBackgroundColor,
+        args: [this.color]
       });
-      // V3 API, uncomment once supported
-      // chrome.scripting.executeScript({
-      //   target: {
-      //     tabId: tabs[0].id!,
-      //   },
-      //   function: () => document.body.style.backgroundColor = this.color
-      // })
+    });
   }
 }
--- src/background.ts
+++ src/background.ts
@@ -1,10 +1,9 @@
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color: '#3aa757' });
-
   chrome.webNavigation.onCompleted.addListener(() => {
     chrome.tabs.query({ active: true, currentWindow: true }, ([{ id }]) => {
       if (id) {
-        chrome.pageAction.show(id);
+        chrome.action.enable();
       }
     });
   }, { url: [{ urlMatches: 'google.com' }] });
